57791ded12e39dbc056adc82531c6379619342de
Sergey5212 <sergey5212@gmail.com>
2023-11-12 19:41:48 +0300

    Delete test_items file

--- test_items.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-
-
-def test_add_to_basket_button_languages(browser):
-    browser.get(link)
-    basket = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
-    assert basket
